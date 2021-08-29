de847126d090f00dec09c5448b888e8ceadb3312
MrJSallivan <justwetwo1@gmail.com>
2021-08-29 18:14:19 +0300

    Homework lesson 3-2

---
+++ lesson32.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+from math import sin, log, e
+
+link="http://suninjuly.github.io/redirect_accept.html"
+
+driver = webdriver.Chrome()
+driver.get(link)
+
+button = driver.find_element_by_class_name("btn")
+button.click()
+
+firsttab=driver.window_handles[0]
+
+driver.switch_to.window(driver.window_handles[1])
+
+x = int(driver.find_element_by_id("input_value").text)
+print(x)
+y = str(log(abs(12*sin(x)), e))
+
+answer=driver.find_element_by_id("answer")
+answer.send_keys(y)
+
+button = driver.find_element_by_class_name("btn")
+button.click()
+
